4db99feb01bb054959fd634752bf19af91cb8c95
VMammedov <vusal.mammadli@unibank.az>
2025-07-21 15:40:03 +0400

    feature/master : Set up CORS config

--- src/main/java/org/project/speakeval/config/SecurityConfig.java
+++ src/main/java/org/project/speakeval/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
 
